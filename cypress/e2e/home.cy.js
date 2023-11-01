fc5bcc4f50d1c30dc3cf8b99733808929c38ea7c
chopper <89061191+Cchopksd@users.noreply.github.com>
2023-11-01 15:07:38 +0700

    first commit

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,20 @@
+// npm run cypress:open
+
+describe('template spec', () => {
+  it('open website', () => {
+
+    cy.visit('https://kabiixoo-z3v6.onrender.com')
+    cy.get('.wel-link-home').click()
+
+    //test search function
+    cy.get('.entry-fill').type('บางขุนเทียน')
+    cy.get('.bt-search').click()
+    cy.get('[href="/provider-profile/0d0a0773-b908-4916-997d-b2d1e6a5b312"]').click()
+
+  })
+  it('single user search function', () => {
+    cy.visit('https://kabiixoo-z3v6.onrender.com/provider-profile/0d0a0773-b908-4916-997d-b2d1e6a5b312')
+    cy.get('.ps-profile-provider-chat-btn').click()
+    cy.get('.ps-profile-provider-report-btn').click()
+  })
+});
